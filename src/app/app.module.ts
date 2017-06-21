e437f25c5c2b04338936da0bb76dc250c0657bae
Rahul Sawant <Rahul.Sawant@salebuild.com>
2017-06-21 11:16:25 -0400

    Added recipe service and shopping-list service files and optimized the recipe component code by applying recipe.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 
+import { RecipeService } from './recipes/recipe.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +32,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
